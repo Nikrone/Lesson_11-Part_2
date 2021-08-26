69e311f89803bff4ad10dd95a06944267c45fe49
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-08-26 16:06:48 +0300

    feat: add part of the Lesson 11

--- Lesson_11-Part_2/ViewController.swift
+++ Lesson_11-Part_2/ViewController.swift
@@ -11,7 +11,53 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let rectangleView = UIView(frame: CGRect(
+                                    x: 30,
+                                    y: 400,
+                                    width: 100,
+                                    height: 100
+            )
+        )
+        rectangleView.backgroundColor = .red
+        view.addSubview(rectangleView)
+        
+//       Создаем анимированный переход (withDuration - сколько анимация займет времени на выполнение),(animations - добавляем анимации, которые хотим совершить)
+        UIView.animate(withDuration: 3) {
+            
+//            перемещение view
+//            rectangleView.center = CGPoint(x: 280, y: 400)
+            
+//            изменение цвета view
+            rectangleView.backgroundColor = .black
+            
+//            перемещение view и изменение размера
+            rectangleView.frame = CGRect(x: 200, y: 200, width: 150, height: 150)
+            
+//            скругление углов view
+            rectangleView.layer.cornerRadius = 75
+            
+//            изменение прозрачности view
+//            rectangleView.alpha = 0.8
+            
+//            повороты view по оси (на количество градусов)
+            rectangleView.transform = CGAffineTransform(rotationAngle: 90)
+//            изменение масштаба view
+            rectangleView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            
+//            В completion - создается новая анимация, когда заканчивается предыдущая!
+//            delay - время до начала анимации
+        } completion: { _ in
+            UIView.animate(withDuration: 2, delay: 1, options: .autoreverse) {
+                rectangleView.backgroundColor = .blue
+                rectangleView.frame = CGRect(x: 100, y: 200, width: 150, height: 150)
+                rectangleView.layer.cornerRadius = 15
+                rectangleView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                rectangleView.transform = CGAffineTransform(rotationAngle: 90)
+            }
+        }
+        
+
     }
 
 
